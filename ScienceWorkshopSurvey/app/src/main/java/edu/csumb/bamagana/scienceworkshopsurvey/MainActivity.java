3827c41163009f0a1f4fa4dce1b785a7a5a5ef14
Brendon Magana <56487444+MRHeavy-G@users.noreply.github.com>
2021-10-01 20:53:51 -0700

    Project Layout_Basic
    Since This was just a basic layout I just added the whole project as a new file. We can remove it once we have a good layout and function for the app

---
+++ ScienceWorkshopSurvey/app/src/main/java/edu/csumb/bamagana/scienceworkshopsurvey/MainActivity.java
@@ -0,0 +1,14 @@
+package edu.csumb.bamagana.scienceworkshopsurvey;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
