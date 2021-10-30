9080a61906e2a143769478c57173d2f69707dc39
Brendon Magana <56487444+MRHeavy-G@users.noreply.github.com>
2021-10-29 22:28:39 -0700

    Worked on adding the checkboxes and and other page.
    The layout of the new page that is added doesn't reflect the final layout. This just a mashup of the inputs. I will work on that futher and then add the camera funcality.

---
+++ ScienceWorkshopSurvey/app/src/main/java/edu/csumb/bamagana/scienceworkshopsurvey/Enjoyment_level.java
@@ -0,0 +1,16 @@
+package edu.csumb.bamagana.scienceworkshopsurvey;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Enjoyment_level extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_enjoyment_level);
+
+
+    }
+}
