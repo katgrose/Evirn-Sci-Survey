358d17440839d711ebdafadc07ccbb114651b2dd
Brendon Magana <56487444+MRHeavy-G@users.noreply.github.com>
2021-12-08 22:15:34 -0800

    Changes to the activities and added the logo back into the app
    Worked on the correction for the activities and worked on adding the images back together. Will work on the video next.

---
+++ ScienceWorkshopSurvey/app/build/generated/source/buildConfig/release/edu/csumb/bamagana/scienceworkshopsurvey/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package edu.csumb.bamagana.scienceworkshopsurvey;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = false;
+  public static final String APPLICATION_ID = "edu.csumb.bamagana.scienceworkshopsurvey";
+  public static final String BUILD_TYPE = "release";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+}
--- app/src/main/java/com/example/evirn_sci_survey/Camera_Interaction.java
+++ app/src/main/java/com/example/evirn_sci_survey/Camera_Interaction.java
@@ -10,17 +10,22 @@ import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.os.Handler;
 import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.Toast;
 
+import com.example.evirn_sci_survey.MainActivity;
 
 
 public class Camera_Interaction extends AppCompatActivity {
 
     private ImageView imgView;
     private Button take_pic;
+    private Button restart_B;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,12 +34,13 @@ public class Camera_Interaction extends AppCompatActivity {
 
         imgView = (ImageView) findViewById(R.id.image_view);
         take_pic = (Button) findViewById(R.id.cam_finish_b);
+        restart_B = (Button) findViewById(R.id.button_restart);
 
         //Request for camera permission
         if(ContextCompat.checkSelfPermission(Camera_Interaction.this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED){
             ActivityCompat.requestPermissions(Camera_Interaction.this,
                     new String[]{
-                      Manifest.permission.CAMERA
+                            Manifest.permission.CAMERA
                     },
 
                     100);
@@ -48,8 +54,21 @@ public class Camera_Interaction extends AppCompatActivity {
             }
         });
 
+        System.out.println("out of image capure and displaying");
+
+        // on waiting for the picture to show up then we cam
+        // once the image is take and displaied for 10 seconds then the survey reverts back to the main page
+        // restartApp();
+
+        restart_B.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Intent i_restart = new Intent(Camera_Interaction.this, MainActivity.class);
+                startActivity(i_restart);
+            }
+        });
     }
-    
+
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
@@ -57,6 +76,19 @@ public class Camera_Interaction extends AppCompatActivity {
             Bitmap camImg = (Bitmap) data.getExtras().get("data");
             imgView.setImageBitmap(camImg);
         }
+    }
+
+    protected void restartApp(){
+
+       /* try {
+            this.wait(10000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }*/
+
+        navigateUpTo(new Intent(this, MainActivity.class));
+        startActivity(getIntent());
+
     }
 
 
